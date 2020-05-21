37953db83c813a1f42eb5c5e50ab94d93f7464ee
Tyler Mapp <tyler@streetmapp.net>
2020-05-21 19:30:52 -0400

    readjusting the sample kpo

--- kube_sample.py
+++ kube_sample.py
@@ -1,100 +1,46 @@
+from airflow import DAG
+from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.contrib.kubernetes.secret import Secret
-from airflow.contrib.kubernetes.volume import Volume
-from airflow.contrib.kubernetes.volume_mount import VolumeMount
-from airflow.contrib.kubernetes.pod import Port
+from airflow import configuration as conf
 
-
-secret_file = Secret('volume', '/etc/sql_conn', 'airflow-secrets', 'sql_alchemy_conn')
-secret_env  = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
-secret_all_keys  = Secret('env', None, 'airflow-secrets-2')
-volume_mount = VolumeMount('test-volume',
-                            mount_path='/root/mount_file',
-                            sub_path=None,
-                            read_only=True)
-port = Port('http', 80)
-configmaps = ['test-configmap-1', 'test-configmap-2']
-
-volume_config= {
-    'persistentVolumeClaim':
-      {
-        'claimName': 'test-volume'
-      }
-    }
-volume = Volume(name='test-volume', configs=volume_config)
-
-affinity = {
-    'nodeAffinity': {
-      'preferredDuringSchedulingIgnoredDuringExecution': [
-        {
-          "weight": 1,
-          "preference": {
-            "matchExpressions": {
-              "key": "disktype",
-              "operator": "In",
-              "values": ["ssd"]
-            }
-          }
-        }
-      ]
-    },
-    "podAffinity": {
-      "requiredDuringSchedulingIgnoredDuringExecution": [
-        {
-          "labelSelector": {
-            "matchExpressions": [
-              {
-                "key": "security",
-                "operator": "In",
-                "values": ["S1"]
-              }
-            ]
-          },
-          "topologyKey": "failure-domain.beta.kubernetes.io/zone"
-        }
-      ]
-    },
-    "podAntiAffinity": {
-      "requiredDuringSchedulingIgnoredDuringExecution": [
-        {
-          "labelSelector": {
-            "matchExpressions": [
-              {
-                "key": "security",
-                "operator": "In",
-                "values": ["S2"]
-              }
-            ]
-          },
-          "topologyKey": "kubernetes.io/hostname"
-        }
-      ]
-    }
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
-tolerations = [
-    {
-        'key': "key",
-        'operator': 'Equal',
-        'value': 'value'
-     }
-]
+namespace = conf.get('kubernetes', 'NAMESPACE')
 
-k = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:16.04",
-                          cmds=["bash", "-cx"],
-                          arguments=["echo", "10"],
-                          labels={"foo": "bar"},
-                          secrets=[secret_file, secret_env, secret_all_keys],
-                          ports=[port]
-                          volumes=[volume],
-                          volume_mounts=[volume_mount]
-                          name="test",
-                          task_id="task",
-                          affinity=affinity,
-                          is_delete_operator_pod=True,
-                          hostnetwork=False,
-                          tolerations=tolerations,
-                          configmaps=configmaps
-                          )
+# This will detect the default namespace locally and read the
+# environment namespace when deployed to Astronomer.
+if namespace =='default':
+    config_file = '/usr/local/airflow/include/.kube/config'
+    in_cluster=False
+else:
+    in_cluster=True
+    config_file=None
 
+dag = DAG('example_kubernetes_pod',
+          schedule_interval='@once',
+          default_args=default_args)
+
+
+compute_resource = {'request_cpu': '800m', 'request_memory': '3Gi', 'limit_cpu': '800m', 'limit_memory': '3Gi'}
+
+with dag:
+    k = KubernetesPodOperator(
+        namespace=namespace,
+        image="hello-world",
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task-one",
+        in_cluster=in_cluster # if set to true, will look in the cluster, if false, looks for file
+        cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
+        config_file=config_file,
+        resources=compute_resource,
+        is_delete_operator_pod=True,
+        get_logs=True)
