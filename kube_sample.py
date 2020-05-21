3a4e2b79ea92b3b73a331838504430a327bbe688
Tyler Mapp <tyler@streetmapp.net>
2020-05-21 19:38:30 -0400

    making some more changes

--- kube_sample.py
+++ kube_sample.py
@@ -6,39 +6,29 @@ from airflow import configuration as conf
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2019, 1, 1),
+    'start_date': datetime.utcnow()
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-namespace = conf.get('kubernetes', 'NAMESPACE')
-
-# This will detect the default namespace locally and read the
-# environment namespace when deployed to Astronomer.
-if namespace =='default':
-    config_file = '/usr/local/airflow/include/.kube/config'
-    in_cluster=False
-else:
-    in_cluster=True
-    config_file=None
 
 dag = DAG('example_kubernetes_pod',
           schedule_interval='@once',
           default_args=default_args)
 
 
-compute_resource = {'request_cpu': '800m', 'request_memory': '3Gi', 'limit_cpu': '800m', 'limit_memory': '3Gi'}
+compute_resource = {'request_cpu': '800m', 'request_memory': '2Gi', 'limit_cpu': '800m', 'limit_memory': '2Gi'}
 
 with dag:
     k = KubernetesPodOperator(
-        namespace=namespace,
+        namespace="airflow",
         image="hello-world",
         labels={"foo": "bar"},
         name="airflow-test-pod",
         task_id="task-one",
-        in_cluster=in_cluster # if set to true, will look in the cluster, if false, looks for file
+        in_cluster=True # if set to true, will look in the cluster, if false, looks for file
         cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
         config_file=config_file,
         resources=compute_resource,
